dc8a2c02f595d0b4f64f7c627be17fe9d5da0478
hudiilfeld <hudifeld@gmail.com>
2018-11-20 02:14:47 +0200

    added begining of  side bar menu functionality

--- ShiurDiario_iOS/ViewController.swift
+++ ShiurDiario_iOS/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sideMenuConstraint: NSLayoutConstraint!
+    
+    var isSideMenuOpened = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(toggleMenu), name: NSNotification.Name("ToggleSideMenu"), object: nil)
     }
+    
+    
+    @objc func toggleMenu() {
+        isSideMenuOpened = !isSideMenuOpened
+        if isSideMenuOpened {
+            UIView.animate(withDuration: 0.4) {
+                self.sideMenuConstraint.constant = -300
+                self.view.layoutIfNeeded()
+            }
+        } else {
+            UIView.animate(withDuration: 0.4) {
+                self.sideMenuConstraint.constant = 0
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
