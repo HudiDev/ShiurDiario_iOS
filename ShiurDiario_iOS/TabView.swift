18d48f99c80fd5ef43fe58be4451ebb051c05158
hudiilfeld <hudifeld@gmail.com>
2018-11-24 20:10:14 +0200

    added protocol for dedection of click events on tabView cells

--- ShiurDiario_iOS/TabView.swift
+++ ShiurDiario_iOS/TabView.swift
@@ -8,14 +8,64 @@
 
 import UIKit
 
+protocol MenuBarDelegate {
+    func menuBarDidSelectItemAt(menu: TabView, index: Int)
+}
+
 class TabView: UIView {
+    
+    @IBOutlet weak var tabsCollectionView: UICollectionView!
+    
+    
+    let titles = ["VIDEO", "GEMARA TEXT", "PREVIOUS DAPIM", "ALL MASECHTOT"]
+    
+    var menuDelegate: MenuBarDelegate?
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSubviews() {
+        tabsCollectionView.delegate = self
+        tabsCollectionView.dataSource = self
+        
+        if let layout = self.tabsCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.estimatedItemSize = UICollectionViewFlowLayoutAutomaticSize
+        }
+    }
+}
 
+
+
+
+extension TabView: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "titleCell", for: indexPath) as! TitleCell
+        
+        cell.titleLabel.text = titles[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        menuDelegate?.menuBarDidSelectItemAt(menu: self, index: indexPath.item)
+        
+        let currentCell = collectionView.cellForItem(at: indexPath) as! TitleCell
+        
+        currentCell.isSelected = true
+    }
 }
