c223d830b910479b697a1076d3e4d7745304198c
hudiilfeld <hudifeld@gmail.com>
2018-11-26 21:00:33 +0200

    pushed PDFDocument process to a background thread with DispatchQueue of GCD and added prefix property to class

--- ShiurDiario_iOS/Page_viewControllers/Text_VC.swift
+++ ShiurDiario_iOS/Page_viewControllers/Text_VC.swift
@@ -12,18 +12,31 @@ import PDFKit
 class Text_VC: UIViewController {
     
     let pdfView = PDFView()
+    var prefix: String?
+    
+    @IBOutlet weak var indicatorBar: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        indicatorBar.startAnimating()
         displayPDF()
     }
     
     func displayPDF() {
-        addConstraints()
-        guard let url = URL(string: "http://shiurdiario.com/media/pdf/Menachot_95.pdf") else { return }
-        if let document = PDFDocument(url: url) {
-            pdfView.document = document
+        DispatchQueue.global(qos: .userInteractive).async {
+            if self.prefix == nil {
+                self.prefix = "Menachot_95"
+            }
+            guard let url = URL(string: "http://shiurdiario.com/media/pdf/\(self.prefix!).pdf") else { return }
+            if let document = PDFDocument(url: url) {
+                DispatchQueue.main.async {
+                    self.indicatorBar.stopAnimating()
+                    self.addConstraints()
+                    print("PDF DATA IS: \(document)")
+                    self.pdfView.document = document
+                    self.pdfView.autoScales = true
+                }
+            }
         }
     }
     
@@ -31,10 +44,10 @@ class Text_VC: UIViewController {
         
         pdfView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(pdfView)
-        pdfView.maxScaleFactor = pdfView.scaleFactorForSizeToFit
+        
         pdfView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        pdfView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-//        pdfView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        //pdfView.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        pdfView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         pdfView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         pdfView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
