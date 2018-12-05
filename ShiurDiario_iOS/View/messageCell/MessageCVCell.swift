0140657df8ec28579352a3b6a590268970fb096d
hudiilfeld <hudifeld@gmail.com>
2018-12-05 02:25:44 +0200

    added msg cell to display error cell to user in Dapim collectionView

---
+++ ShiurDiario_iOS/View/messageCell/MessageCVCell.swift
@@ -0,0 +1,19 @@
+//
+//  MessageCVCell.swift
+//  ShiurDiario_iOS
+//
+//  Created by Hudi Ilfeld on 12/4/18.
+//  Copyright © 2018 Hudi Ilfeld. All rights reserved.
+//
+
+import UIKit
+
+class MessageCVCell: UICollectionViewCell {
+    
+    @IBOutlet weak var msgLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
